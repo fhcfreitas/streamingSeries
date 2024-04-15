4f3c28541819b35da5dd18d93390a2b37e77e4d4
Fabio Freitas <fhcfreitas@gmail.com>
2024-04-15 20:13:22 -0300

    front end and series details

--- src/main/java/fhcfreitas/java/demo/DemoApplication.java
+++ src/main/java/fhcfreitas/java/demo/DemoApplication.java
@@ -1,32 +1,17 @@
 package fhcfreitas.java.demo;
 
 import fhcfreitas.java.demo.main.Main;
-import fhcfreitas.java.demo.model.EpisodeData;
-import fhcfreitas.java.demo.model.SeasonData;
-import fhcfreitas.java.demo.model.SeriesData;
 import fhcfreitas.java.demo.repository.SeriesRepository;
-import fhcfreitas.java.demo.service.ConsumingAPI;
-import fhcfreitas.java.demo.service.ConvertData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-	@Autowired
-	private SeriesRepository repository;
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Main main = new Main(repository);
-		main.menu();
-	}
 }
